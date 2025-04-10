e48f5c849d8b8b3c34a5132275401b8ffa4c6e75
cristobal <c.ramos07@ufromail.cl>
2025-04-10 10:31:51 -0400

    Añadiendo mostrar fila

--- src/matrices.java
+++ src/matrices.java
@@ -21,12 +21,12 @@ public class matrices {
     private static void mostrarOpciones() {
         System.out.println("\n============================" );
         System.out.println("            MENÚ              " );
-        System.out.println("      Algunas cosillas        " );
+        System.out.println("      matrices        " );
         System.out.println("============================= " );
-        System.out.println("    [1] Agregar               " );
-        System.out.println("    [2] Eliminar              " );
-        System.out.println("    [3] Listar                " );
-        System.out.println("    [4] Buscar                " );
+        System.out.println("    [1] crear matriz             " );
+        System.out.println("    [2] llenar matriz             " );
+        System.out.println("    [3] mostrar fila             " );
+        System.out.println("    [4] verificar matriz cero            " );
         System.out.println("    [5] Salir                 " );
         System.out.println("============================= " );
         System.out.print("      Opcion: ");
@@ -37,9 +37,9 @@ public class matrices {
 
     private static void ejecutarOpcion(int opcion) {
         switch (opcion) {
-            case 1 -> System.out.println(" 1");
-            case 2 -> System.out.println(" 2");
-            case 3 -> System.out.println(" 3");
+            case 1 -> crearMatriz();
+            case 2 -> llenarMatriz();
+            case 3 -> mostrarFila(,preguntarFila());
             case 4 -> System.out.println(" 4");
             case 5 -> System.out.println("  Hasta luego...");
             default -> System.out.println(" Opción invalida....");
@@ -54,7 +54,7 @@ public class matrices {
         }
     }
 
-    int [ ][ ]crearMatriz(int filas, int cols){
+    static int [ ][ ]crearMatriz(int filas, int cols){
         int[][] matriz = new int[filas][cols];
         System.out.println("Introduce los valores de la matriz :");
         for (int i = 0; i < filas; i++) {
@@ -65,7 +65,7 @@ public class matrices {
         }
         return matriz;
     }
-    int [][]llenarMatriz(int matriz [][] ) {
+    static int [][]llenarMatriz(int matriz [][] ) {
         Random random = new Random();
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[0].length; j++) {
@@ -75,6 +75,23 @@ public class matrices {
         return matriz;
     }
 
+    static void mostrarFila(int matriz[ ][ ],int fila) {
+        for (int i = 0; i < matriz[fila].length; i++) {
+            System.out.println(matriz[fila][i] + " ");
+        }
+    }
+    static int preguntarFila(){
+        System.out.println("ingrese el numero de fila que desea ver");
+        int fila = scanner.nextInt();
+        return fila;
+    }
 
+    boolean matrizCero(int matriz[][]){
+        int contador = 0;
+        for (int i=0;i< matriz.length;i++){
+            for(int j=0;i< matriz[0].length;i++){
 
+            }
+        }
+    }
 }
